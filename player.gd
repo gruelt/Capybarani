91410641f1fb42cd5d64361c182f9dce10f1e6c5
Thomas GRUEL <gruel@emse.Fr>
2026-01-15 11:46:26 +0100

    added capybara

--- player.gd
+++ player.gd
@@ -9,6 +9,11 @@ var boost_factor = 1.5  # Multiplicateur si on saute au bon moment
 var rotation_speed = 5.0
 var tucked_multiplier = 2.5 # Vitesse de rotation quand on est "groupé"
 
+func start(pos):
+	position = pos
+	#$CollisionShape2D.disabled = false
+
+
 func _physics_process(delta):
 	# 1. Gestion de la Gravité
 	if not is_on_floor():
@@ -16,7 +21,7 @@ func _physics_process(delta):
 	
 	# 2. Gestion du Saut et du Timing
 	if is_on_floor():
-		if Input.is_action_just_pressed("jump"): # "Espace" par défaut
+		if Input.is_action_pressed("jump"): # "Espace" par défaut
 			# Saut boosté si on appuie pile au contact
 			velocity.y = jump_force * boost_factor
 		else:
@@ -27,7 +32,7 @@ func _physics_process(delta):
 	var current_rot_speed = rotation_speed
 	
 	# Si on appuie sur "Bas", on tourne plus vite
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("group"):
 		current_rot_speed *= tucked_multiplier
 		scale = Vector2(0.7, 0.7) # Effet visuel "groupé"
 	else:
